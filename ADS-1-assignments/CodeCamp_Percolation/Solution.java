488ff2a611295073cf5a130d01a8f4679ab6a8a1
navin106 <sandula.navin@msitprogram.net>
2018-09-25 19:20:06 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-1-assignments/CodeCamp_Percolation/Solution.java
+++ ADS-1-assignments/CodeCamp_Percolation/Solution.java
@@ -1,11 +1,11 @@
 // public class Percolation {
-//    public Percolation(int n)                
+//    public Percolation(int n)
 // create n-by-n grid, with all sites blocked
-//    public    void open(int row, int col)    
+//    public    void open(int row, int col)
 // open site (row, col) if it is not open already
-//    public boolean isOpen(int row, int col)  
+//    public boolean isOpen(int row, int col)
 // is site (row, col) open?
-//    public boolean isFull(int row, int col)  
+//    public boolean isFull(int row, int col)
 // is site (row, col) full?
 //    public     int numberOfOpenSites()
 // number of open sites
@@ -19,7 +19,12 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
 		Percolation p = new Percolation(a);
