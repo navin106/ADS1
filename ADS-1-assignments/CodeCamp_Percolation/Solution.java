c5beac0d0b083f5d6e765901f07aeb9e2db940df
navin106 <sandula.navin@msitprogram.net>
2018-09-25 19:18:57 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-1-assignments/CodeCamp_Percolation/Solution.java
+++ ADS-1-assignments/CodeCamp_Percolation/Solution.java
@@ -1,14 +1,23 @@
 // public class Percolation {
-//    public Percolation(int n)                // create n-by-n grid, with all sites blocked
-//    public    void open(int row, int col)    // open site (row, col) if it is not open already
-//    public boolean isOpen(int row, int col)  // is site (row, col) open?
-//    public boolean isFull(int row, int col)  // is site (row, col) full?
-//    public     int numberOfOpenSites()       // number of open sites
-//    public boolean percolates()              // does the system percolate?
+//    public Percolation(int n)                
+// create n-by-n grid, with all sites blocked
+//    public    void open(int row, int col)    
+// open site (row, col) if it is not open already
+//    public boolean isOpen(int row, int col)  
+// is site (row, col) open?
+//    public boolean isFull(int row, int col)  
+// is site (row, col) full?
+//    public     int numberOfOpenSites()
+// number of open sites
+//    public boolean percolates()
+// does the system percolate?
 // }
 // You can implement the above API to solve the problem
 
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -17,6 +26,6 @@ public class Solution {
 		while (s.hasNext()) {
 			p.open(s.nextInt(), s.nextInt());
 		}
-			System.out.println(p.percolates());
+		System.out.println(p.percolates());
 	}
 }
