fff272ee8240b0e458a6001f97d5f822e4023d8d
navin106 <sandula.navin@msitprogram.net>
2018-09-26 16:03:43 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m3/3-sum/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class ThreeSum {
+	int cnt = 0;
+	int noOfTrips(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j=i+2; j<arr.length; j++) {
+				if(arr[i] + arr[i+1] == arr[j]) {
+					cnt++;
+				}
+			}
+
+		}
+		return cnt;
+
+	}
+}
+class CreateArray {
+	private int[] arr;
+	ThreeSum ts = new ThreeSum();
+	public CreateArray(int k, Scanner sc) {
+		arr = new int[k];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(ts.noOfTrips(arr));
+	}
+}
+public final class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
+		CreateArray ts = new CreateArray(k, sc);
+	}
+
+}
