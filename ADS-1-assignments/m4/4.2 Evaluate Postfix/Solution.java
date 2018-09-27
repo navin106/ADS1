e9277787e71576a6cd86f04d12955c663b61d723
navin106 <sandula.navin@msitprogram.net>
2018-09-27 22:05:51 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1-assignments/m4/4.2 Evaluate Postfix/Solution.java
@@ -1,35 +1,41 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	private Solution() {
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String[] s = sc.nextLine().split(" ");
-		Mystack<Integer> stack = new Mystack<Integer>();
-		for (int i = 0; i < s.length; i++) {
-			if (s[i].equals("*") || s[i].equals(
-				"/") || s[i].equals("+") || s[i].equals("-")) {
-				if (s[i].equals("*")) {
-					stack.push(stack.pop() * stack.pop());
-				}
-				if (s[i].equals("/")) {
-					stack.push(stack.pop() / stack.pop());
-				}
-				if (s[i].equals("+")) {
-					stack.push(stack.pop() + stack.pop());
-				}
-				if (s[i].equals("-")) {
-					stack.push(stack.pop() - stack.pop());
-				}
-			} else {
-				stack.push(Integer.parseInt(s[i]));
-			}
-		}
-		System.out.println(stack.pop());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split(" ");
+        Mystack<Integer> stack = new Mystack<Integer>();
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("*") || s[i].equals(
+                "/") || s[i].equals("+") || s[i].equals("-")) {
+                if (s[i].equals("*")) {
+                    stack.push(stack.pop() * stack.pop());
+                }
+                if (s[i].equals("/")) {
+                    stack.push(stack.pop() / stack.pop());
+                }
+                if (s[i].equals("+")) {
+                    stack.push(stack.pop() + stack.pop());
+                }
+                if (s[i].equals("-")) {
+                    stack.push(stack.pop() - stack.pop());
+                }
+            } else {
+                stack.push(Integer.parseInt(s[i]));
+            }
+        }
+        System.out.println(stack.pop());
+    }
 }
