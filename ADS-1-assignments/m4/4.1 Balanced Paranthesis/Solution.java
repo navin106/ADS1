c5f61aed7959bc9abb07fa0f922d687d24987a2c
navin106 <sandula.navin@msitprogram.net>
2018-09-27 22:36:23 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public final class Solution {
+	private Solution() {
+	}
+	public static void main(final String[] args) {
+		int cnt = 0;
+		Scanner sc = new Scanner(System.in);
+		Mystack<String> ms = new Mystack<String>();
+		int k = sc.nextInt();
+		while (sc.hasNext()) {
+			String[] expr = sc.nextLine().split("");
+			for (int i = 0; i < expr.length; i++) {
+				if (expr[i].equals("[")) { ms.push(expr[i]); cnt++; }
+				else { if (expr[i] != "]") break; }
+				if (expr[i].equals("(")) { ms.push(expr[i]); cnt++; }
+				else { if (expr[i] != ")") break; }
+				if (expr[i].equals("{")) { ms.push(expr[i]); cnt++; }
+				else { if (expr[i] != "}") break; }
+			}
+		}
+		if (ms.size == 0) { System.out.println("YES"); }
+		else { System.out.println("NO"); }
+	}
+}
