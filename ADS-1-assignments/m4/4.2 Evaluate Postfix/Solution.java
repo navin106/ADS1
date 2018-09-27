9790e08acc7e502339b9ebadb8e3dd621a33e293
navin106 <sandula.navin@msitprogram.net>
2018-09-27 22:05:10 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1-assignments/m4/4.2 Evaluate Postfix/Solution.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 public class Solution {
-	public static void main(String[] args) {
+	private Solution() {
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] s = sc.nextLine().split(" ");
 		Mystack<Integer> stack = new Mystack<Integer>();
 		for (int i = 0; i < s.length; i++) {
-			if (s[i].equals("*") || s[i].equals("/") || s[i].equals("+") || s[i].equals("-")) {
+			if (s[i].equals("*") || s[i].equals(
+				"/") || s[i].equals("+") || s[i].equals("-")) {
 				if (s[i].equals("*")) {
 					stack.push(stack.pop() * stack.pop());
 				}
