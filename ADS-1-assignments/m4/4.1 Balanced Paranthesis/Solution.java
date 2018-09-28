98b6fd899779715d5b29a7bd4ace0097bbe02ecd
navin106 <sandula.navin@msitprogram.net>
2018-09-28 09:40:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -11,19 +11,15 @@ public final class Solution {
 			int flag = 0;
 			int cnt = 0;
 			String[] expr = sc.next().split("");
-			if (expr[0].equals("]") || expr[0].equals(
-				")") || expr[0].equals("}")) System.out.println("NO");
+			if (expr[0].equals("]") || expr[0].equals(")") || expr[0].equals("}")) System.out.println("NO");
 			else {
 				for (int i = 0; i < expr.length; i++) {
-					if (expr[i].equals("[")) { ms.push(expr[i]); cnt++;}
-					if (expr[i].equals("]")) { if (ms.size > 0) {
-					 if (ms.peek().equals("[")) ms.pop(); else flag = 1; } }
+					if (expr[i].equals("[")) {ms.push(expr[i]); cnt++;}
+					if (expr[i].equals("]")) { if (ms.size > 0) {if (ms.peek().equals("[")) ms.pop(); else flag = 1; } }
 					if (expr[i].equals("(")) { ms.push(expr[i]); cnt++;}
-					if (expr[i].equals(")")) { if (ms.size > 0) {
-					 if (ms.peek().equals("(")) ms.pop(); else flag = 1; } }
+					if (expr[i].equals(")")) { if (ms.size > 0) {if (ms.peek().equals("(")) ms.pop(); else flag = 1; } }
 					if (expr[i].equals("{")) { ms.push(expr[i]); cnt++;}
-					if (expr[i].equals("}")) { if (ms.size > 0) {
-					 if (ms.peek().equals("{")) ms.pop(); else flag = 1; } }
+					if (expr[i].equals("}")) { if (ms.size > 0) {if (ms.peek().equals("{")) ms.pop(); else flag = 1; } }
 				}
 				if (flag == 0 && cnt == expr.length/2) {
 				 System.out.println("YES"); }
