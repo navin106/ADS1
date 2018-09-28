1e8ac3b2201fb046dc1043b6ba3df04643643d2d
navin106 <sandula.navin@msitprogram.net>
2018-09-28 18:27:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS-1-assignments/m5/5.1 Deque/Solution.java
+++ ADS-1-assignments/m5/5.1 Deque/Solution.java
@@ -2,52 +2,56 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		MyQueue mq =  new MyQueue();
-		int k = sc.nextInt();
-		while (sc.hasNext()) {
-			String[] token = sc.nextLine().split(" ");
-			switch (token[0]) {
-			case "pushLeft":
-				mq.pushLeft(token[1]);
-				System.out.println(mq);
-				break;
-			case "pushRight":
-				mq.pushRight(token[1]);
-				System.out.println(mq);
-				break;
-			case "popLeft":
-				if (mq.size > 0) {
-					mq.popLeft();
-					System.out.println(mq);
-				} else { System.out.println("Deck is empty"); }
-				break;
-			case "popRight":
-				if (mq.size > 0) {
-					mq.popRight();
-					System.out.println(mq);
-				} else { System.out.println("Deck is empty"); }
-				break;
-			case "size":
-				System.out.println(mq.size());
-				break;
-			default:
-				break;
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyQueue mq =  new MyQueue();
+        int k = sc.nextInt();
+        while (sc.hasNext()) {
+            String[] token = sc.nextLine().split(" ");
+            switch (token[0]) {
+            case "pushLeft":
+                mq.pushLeft(token[1]);
+                System.out.println(mq);
+                break;
+            case "pushRight":
+                mq.pushRight(token[1]);
+                System.out.println(mq);
+                break;
+            case "popLeft":
+                if (mq.size > 0) {
+                    mq.popLeft();
+                    System.out.println(mq);
+                } else {
+                    System.out.println("Deck is empty");
+                }
+                break;
+            case "popRight":
+                if (mq.size > 0) {
+                    mq.popRight();
+                    System.out.println(mq);
+                } else {
+                    System.out.println("Deck is empty");
+                }
+                break;
+            case "size":
+                System.out.println(mq.size());
+                break;
+            default:
+                break;
 
-			}
+            }
 
-		}
-	}
+        }
+    }
 }
