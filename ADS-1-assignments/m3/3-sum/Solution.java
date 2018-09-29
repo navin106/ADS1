273313de0d117f9971e013635196a62f9d3bad34
navin106 <sandula.navin@msitprogram.net>
2018-09-29 19:13:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m3/3-sum/Solution.java
+++ ADS-1-assignments/m3/3-sum/Solution.java
@@ -1,35 +1,72 @@
 import java.util.Scanner;
-class ThreeSum {
-	int cnt = 0;
-	int noOfTrips(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i; j < arr.length; j++) {
-				for (int k = j; k < arr.length; k/=2) {
-					if (arr[i] + arr[j] + arr[k] == 0) {
-						cnt++;
-					}
+import java.util.Arrays;
+/**
+ * @author : Navin106.
+ * This program looks for the triplets which adds to 0 in a given array.
+ */
+/**
+ * Class for three sum.
+ */
+final class ThreeSumClass {
+	/**
+	 * Constructs the object.
+	 */
+	private ThreeSumClass() {
+		// unused constructor
+	}
+
+	/**
+	 * Counts the number of triplets whose sum is zero in the given array.
+	 *
+	 * @param      arr   The array.
+	 *
+	 * @return     Number of triplets.
+	 */
+	public static int countNoOfTriplets(final int[] arr) {
+		int count = 0;
+		Arrays.sort(arr);
+		for (int k = 0; k < arr.length; k++) {
+			int i = 0;
+			int j = arr.length - 1;
+			int element = arr[k];
+			while (i != k && j != k && i < j) {
+				int sum = arr[i] + arr[j];
+				if (sum + element > 0) {
+					j -= 1;
+				} else if (sum + element < 0) {
+					i += 1;
+				} else {
+					count += 1;
+					i++;
 				}
 			}
 		}
-		return cnt;
+		return count;
 	}
 }
-class CreateArray {
-	private int[] arr;
-	ThreeSum ts = new ThreeSum();
-	public CreateArray(int k, Scanner sc) {
-		arr = new int[k];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (sc.nextInt());
-		}
-		System.out.println(ts.noOfTrips(arr));
-	}
-}
-public final class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int k = sc.nextInt();
-		CreateArray ts = new CreateArray(k, sc);
-	}
 
+/**
+ * Class for Solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		// unused constructor
+	}
+	/**
+	 * Main function: Reads in the input and prints out the # of triplets.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		int[] inpArr = new int[num];
+		for (int i = 0; i < num; i++) {
+			inpArr[i] = scan.nextInt();
+		}
+		System.out.println(ThreeSumClass.countNoOfTriplets(inpArr));
+	}
 }
