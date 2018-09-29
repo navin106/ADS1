5f5dbae710188f9f232d7fc685e0ce7204317f90
navin106 <sandula.navin@msitprogram.net>
2018-09-29 19:28:47 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS-1-assignments/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1-assignments/m5/5.2 Josephus Problem/Solution.java
@@ -1,20 +1,112 @@
-import java.util.Scanner;
-public final class Solution {
-	Solution() {
-
-	}
+import java.util.*;
+public class Solution {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		MyQueue mq = new MyQueue();
-		int k = sc.nextInt();
-		while (sc.hasNext()) {
-			String[] token = sc.nextLine().split(" ");
-			int cnt = 1;
-			for (int i = 0; i < token.length; i++) { mq.pushLeft(token[i]); }
-			for (int i = 0; i < token.length; i++) {
-				if (cnt % 2 == 0) { System.out.println(mq.popRight());}
-				else mq.pushLeft(mq.popRight());
-			}
+		Scanner scan = new Scanner(System.in);
+		String testcases = scan.nextLine();
+		while (scan.hasNext()) {
+			String[] line = scan.nextLine().split(" ");
+			int people = Integer.parseInt(line[0]);
+			int gap = Integer.parseInt(line[1]);
+			Deque<Integer> q = new Deque<>();
+			createCircle(q, people);
+			q.josephus(people, gap);
 		}
 	}
+	public static void createCircle(Deque<Integer> q, int people) {
+		for (int i = 0; i < people; i++) {
+			q.push(i);
+		}
+	}
+}
+class Deque<Item> {
+	Node first = null;
+	Node last = null;
+	int size = 0;
+	String print = "";
+	class Node {
+		Item data;
+		Node next;
+		Node(Item data) {
+			this.data = data;
+		}
+	}
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void push(Item element) {
+		Node oldleft = last;
+		last = new Node(element);
+		if (isEmpty()) {
+			first = last;
+			last.next = first;
+			size++;
+			return;
+		}
+		last.next = first;
+		oldleft.next = last;
+		size++;
+	}
+	public Item pop(Item person) {
+		Item d = first.data;
+		if (isEmpty()) {
+			return null;
+		}
+		int s = 0;
+		Node get = first;
+		Node prev = null;
+		while (s < size) {
+			if (get.data == person) {
+				if (first.data == person) {
+					first = first.next;
+					int g = 0;
+					Node l = first;
+					while (g < size - 2) {
+						l = l.next;
+						g++;
+					}
+					last = l;
+					last.next = first;
+				} else {
+					prev.next = get.next;
+				}
+				size--;
+				break;
+			}
+			prev = get;
+			get = get.next;
+			s++;
+		}
+		return d;
+	}
+	public void josephus(int people, int gap) {
+		Node head = first;
+		while (!isEmpty()) {
+			int c = 0;
+			while (c < gap - 1) {
+				head = head.next;
+				c++;
+			}
+			print += head.data + " ";
+			pop(head.data);
+			head = head.next;
+		}
+		print = print.trim();
+		System.out.println(print);
+	}
+	public void printList() {
+		Node n = first;
+		Node tnode = first.next;
+		String s = "";
+		s += n.data + " ";
+		while (tnode != first) {
+			s += tnode.data + " ";
+			tnode = tnode.next;
+		}
+		s = s.trim();
+		System.out.println(s);
+	}
+
 }
