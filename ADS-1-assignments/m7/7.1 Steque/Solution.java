4f97397636fb7f68c22424fc00017e715db2b13f
navin106 <sandula.navin@msitprogram.net>
2018-10-01 19:35:43 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1-assignments/m7/7.1 Steque/Solution.java
+++ ADS-1-assignments/m7/7.1 Steque/Solution.java
@@ -1,109 +1,109 @@
 import java.util.Scanner;
 import java.util.Arrays;
 class Steque<E> {
-	int size = 0;
-	private class Node {
-		E data;
-		Node next;
-	}
-	private Node head;
-	private Node tail;
+    int size = 0;
+    private class Node {
+        E data;
+        Node next;
+    }
+    private Node head;
+    private Node tail;
 
-	public void push(E data) {
-		Node node = new Node();
-		node.data = data;
-		node.next = head;
-		if (head == null) {
-			head = node;
-			tail = head;
-			size++;
-			return;
-		}
-		head = node;
-		size++;
-	}
+    public void push(E data) {
+        Node node = new Node();
+        node.data = data;
+        node.next = head;
+        if (head == null) {
+            head = node;
+            tail = head;
+            size++;
+            return;
+        }
+        head = node;
+        size++;
+    }
 
-	public void enqueue(E data) {
-		Node node = new Node();
-		if (head == null) {
-			push(data);
-		} else {
-			node.data = data;
-			node.next = null;
-			tail.next = node;
-			tail = node;
-			size++;
-		}
-	}
+    public void enqueue(E data) {
+        Node node = new Node();
+        if (head == null) {
+            push(data);
+        } else {
+            node.data = data;
+            node.next = null;
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+    }
 
-	public E pop() throws Exception {
-		if (size == 0) {
-			throw new Exception();
-		}
-		E data = head.data;
-		head = head.next;
-		size--;
-		return data;
-	}
-	public int size() {
-		return size;
-	}
-	public String toString() {
-		String str = "";
-		Node thead = head;
-		if (thead != null) {
-			while (thead.next != null) {
-				str += thead.data + ", ";
-				thead = thead.next;
-			}
-			str += thead.data;
+    public E pop() throws Exception {
+        if (size == 0) {
+            throw new Exception();
+        }
+        E data = head.data;
+        head = head.next;
+        size--;
+        return data;
+    }
+    public int size() {
+        return size;
+    }
+    public String toString() {
+        String str = "";
+        Node thead = head;
+        if (thead != null) {
+            while (thead.next != null) {
+                str += thead.data + ", ";
+                thead = thead.next;
+            }
+            str += thead.data;
 
-		} else {
-			return "Steque is empty.";
-		}
-		str += "";
-		return str;
+        } else {
+            return "Steque is empty.";
+        }
+        str += "";
+        return str;
 
-	}
+    }
 
 }
 public class Solution {
-	public Solution() {
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int no = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i < no + 1; i++) {
-			Steque<String> stq = new Steque<String>();
-			while (sc.hasNext()) {
-				String t = sc.nextLine();
-				if (t.equals("")) {
-					break;
-				}
-				String[] tokens = t.split(" ");
-				switch (tokens[0]) {
-				case "push":
-					stq.push(tokens[1]);
-					System.out.println(stq);
-					break;
-				case "pop":
-					try {
-						stq.pop();
-						System.out.println(stq);
-					} catch (Exception e) {
-						System.out.println("Steque is empty.");
-					}
-					break;
-				case "enqueue":
-					stq.enqueue(tokens[1]);
-					System.out.println(stq);
-					break;
-				default:
-					break;
-				}
-			}
-			System.out.println();
-		}
-	}
+    public Solution() {
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int no = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < no + 1; i++) {
+            Steque<String> stq = new Steque<String>();
+            while (sc.hasNext()) {
+                String t = sc.nextLine();
+                if (t.equals("")) {
+                    break;
+                }
+                String[] tokens = t.split(" ");
+                switch (tokens[0]) {
+                case "push":
+                    stq.push(tokens[1]);
+                    System.out.println(stq);
+                    break;
+                case "pop":
+                    try {
+                        stq.pop();
+                        System.out.println(stq);
+                    } catch (Exception e) {
+                        System.out.println("Steque is empty.");
+                    }
+                    break;
+                case "enqueue":
+                    stq.enqueue(tokens[1]);
+                    System.out.println(stq);
+                    break;
+                default:
+                    break;
+                }
+            }
+            System.out.println();
+        }
+    }
 }
