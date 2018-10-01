cd4761bfe553b7e1f595a251c92f8f13e1cccbee
navin106 <sandula.navin@msitprogram.net>
2018-10-01 18:20:49 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- ADS-1-assignments/m7/7.1 Steque/Solution.java
+++ ADS-1-assignments/m7/7.1 Steque/Solution.java
@@ -89,11 +89,11 @@ public class Solution {
 					System.out.println(stq);
 					break;
 				case "pop":
+					stq.pop();
 					if (stq.size == 0) {
 						System.out.println("Steque is empty.");
 						break;
 					}
-					stq.pop();
 					System.out.println(stq);
 					break;
 				case "enqueue":
