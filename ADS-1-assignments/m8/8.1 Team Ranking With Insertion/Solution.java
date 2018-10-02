fbe48b7f9c38b157f597741ecc17c7726a40cf0b
navin106 <sandula.navin@msitprogram.net>
2018-10-02 19:05:18 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] names = new String[args.length];
+		int size = 0;
+		while (sc.hasNext()) {
+			String[] k = sc.nextLine().split(",");
+			names[size++] = k[0];
+		}
+	}
+	void Sort(Comparable[] a) {
+		String str = "";
+		int N = a.length;
+		for (int i = 0; i < N; i++) {
+			int min = i;
+			for (int j = i + 1; j < N; j++)
+				if (less(a[j], a[min]))
+					min = j;
+			exch(a, i, min);
+		}
+		for (Comparable k : a) {
+			str += k + ",";
+		}
+		System.out.println(str.substring(0,a.length-2));
+	}
+	private static boolean less(Comparable v, Comparable w)
+	{ return v.compareTo(w) < 0; }
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
+}
