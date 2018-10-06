4ac3f12958df987dd0d784ff5cf72a6432d097cf
navin106 <sandula.navin@msitprogram.net>
2018-10-07 00:08:02 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m12/Assignment-1/Solution.java
+++ ADS-1-assignments/m12/Assignment-1/Solution.java
@@ -77,7 +77,7 @@ class Student implements Comparable<Student> {
 		} else if (Integer.parseInt(this.getmark1()) < Integer.parseInt(that.getmark1())) {
 			return -1;
 		}else if (this.getdateofbirth().compareTo(that.getdateofbirth()) > 0) {
-			return -1;
+			return 1;
 		}
 		 else {
 			return 0;
