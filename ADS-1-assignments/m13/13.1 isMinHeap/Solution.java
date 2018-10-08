aa36b1cb2e1307c1c6a6581270aba66191c89683
navin106 <sandula.navin@msitprogram.net>
2018-10-08 16:32:48 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m13/13.1 isMinHeap/Solution.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+class IsMinHeap<E> {
+	public IsMinHeap() {
+
+	}
+	public boolean checkminheap(Comparable[] array) {
+		boolean flag = true;
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i].compareTo(array[i + 1]) < 0) {
+				flag = false;
+				return flag;
+			}
+
+		}
+		return flag;
+	}
+
+}
+public class Solution<E> {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String k = sc.next();
+		int no = sc.nextInt();
+		sc.nextLine();
+		while (sc.hasNext()) {
+			String[] inp = sc.nextLine().split(",");
+			switch (k) {
+			case "String" :
+				IsMinHeap<String> datatype = new IsMinHeap<>();
+				System.out.println(datatype.checkminheap(inp));
+				break;
+			case "Double":
+				IsMinHeap<Double> ddatatype = new IsMinHeap<>();
+				Double[] dinp = Arrays.stream(inp).map(Double::valueOf).toArray(Double[]::new);
+				System.out.println(ddatatype.checkminheap(dinp));
+				break;
+			case "Integer":
+				IsMinHeap<Integer> idatatype = new IsMinHeap<>();
+				Integer[] iinp = Arrays.stream(inp).map(Integer::valueOf).toArray(Integer[]::new);
+				System.out.println(idatatype.checkminheap(iinp));
+				break;
+			case "Float":
+				IsMinHeap<Float> fdatatype = new IsMinHeap<>();
+				Float[] finp = Arrays.stream(inp).map(Float::valueOf).toArray(Float[]::new);
+				System.out.println(fdatatype.checkminheap(finp));
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
