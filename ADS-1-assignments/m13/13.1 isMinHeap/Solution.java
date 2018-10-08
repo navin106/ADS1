e3d7927d81319091b119855c06a2631b5e5bd220
navin106 <sandula.navin@msitprogram.net>
2018-10-08 16:57:58 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-assignments/m13/13.1 isMinHeap/Solution.java
+++ ADS-1-assignments/m13/13.1 isMinHeap/Solution.java
@@ -6,101 +6,101 @@ import java.util.Arrays;
  * @param      <E>   { parameter_description }
  */
 class IsMinHeap<E> {
-	/**
-	 * Constructs the object.
-	 */
-	IsMinHeap() {
+    /**
+     * Constructs the object.
+     */
+    IsMinHeap() {
 
-	}
-	/**
-	 * checkminheap.
-	 * complexity is N
-	 *
-	 * @param      array  The array
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public boolean checkminheap(final Comparable[] array) {
-		boolean flag = true;
-		if (array.length == 0) {
-			return false;
-		}
-		for (int i = 0; i < array.length - 1; i++) {
-			if (!(array[i].compareTo(array[i + 1]) <= 0)) {
-				flag = false;
-				return flag;
-			}
+    }
+    /**
+     * checkminheap.
+     * complexity is N
+     *
+     * @param      array  The array
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean checkminheap(final Comparable[] array) {
+        boolean flag = true;
+        if (array.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            if (!(array[i].compareTo(array[i + 1]) <= 0)) {
+                flag = false;
+                return flag;
+            }
 
-		}
-		return flag;
-	}
+        }
+        return flag;
+    }
 
 }
 /**
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String k = sc.next();
-		int no = sc.nextInt();
-		sc.nextLine();
-		while (no > 0) {
-			String inp1 = sc.nextLine();
-			String[] inp = inp1.split(",");
-			if (inp1.length() > 0) {
-				switch (k) {
-				case "String" :
-					IsMinHeap
-					<String> datatype = new IsMinHeap<>();
-					System.out.println(datatype
-						.checkminheap(inp));
-					break;
-				case "Double":
-					IsMinHeap
-					<Double> ddatatype = new IsMinHeap<>();
-					Double[] dinp = Arrays.stream(inp)
-					.map(Double::valueOf)
-					.toArray(Double[]::new);
-					System.out.println(ddatatype
-						.checkminheap(dinp));
-					break;
-				case "Integer":
-					IsMinHeap
-					<Integer> idatatype = new IsMinHeap<>();
-					Integer[] iinp = Arrays.stream(inp)
-					.map(Integer::valueOf)
-					.toArray(Integer[]::new);
-					System.out.println(idatatype
-						.checkminheap(iinp));
-					break;
-				case "Float":
-					IsMinHeap
-					<Float> fdatatype = new IsMinHeap<>();
-					Float[] finp = Arrays.stream(inp)
-					.map(Float::valueOf)
-					.toArray(Float[]::new);
-					System.out.println(fdatatype
-						.checkminheap(finp));
-					break;
-				default:
-					break;
-				}
-			} else {
-				System.out.println("false");
-			}
-			no--;
-		}
-	}
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String k = sc.next();
+        int no = sc.nextInt();
+        sc.nextLine();
+        while (no > 0) {
+            String inp1 = sc.nextLine();
+            String[] inp = inp1.split(",");
+            if (inp1.length() > 0) {
+                switch (k) {
+                case "String" :
+                    IsMinHeap
+                    <String> datatype = new IsMinHeap<>();
+                    System.out.println(datatype
+                        .checkminheap(inp));
+                    break;
+                case "Double":
+                    IsMinHeap
+                    <Double> ddatatype = new IsMinHeap<>();
+                    Double[] dinp = Arrays.stream(inp)
+                    .map(Double::valueOf)
+                    .toArray(Double[]::new);
+                    System.out.println(ddatatype
+                        .checkminheap(dinp));
+                    break;
+                case "Integer":
+                    IsMinHeap
+                    <Integer> idatatype = new IsMinHeap<>();
+                    Integer[] iinp = Arrays.stream(inp)
+                    .map(Integer::valueOf)
+                    .toArray(Integer[]::new);
+                    System.out.println(idatatype
+                        .checkminheap(iinp));
+                    break;
+                case "Float":
+                    IsMinHeap
+                    <Float> fdatatype = new IsMinHeap<>();
+                    Float[] finp = Arrays.stream(inp)
+                    .map(Float::valueOf)
+                    .toArray(Float[]::new);
+                    System.out.println(fdatatype
+                        .checkminheap(finp));
+                    break;
+                default:
+                    break;
+                }
+            } else {
+                System.out.println("false");
+            }
+            no--;
+        }
+    }
 }
