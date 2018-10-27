50b345115eea7dbacd966e33930ff870c0d5feb2
navin106 <sandula.navin@msitprogram.net>
2018-10-27 12:20:52 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m24/2/Solution.java
+++ ADS-1-assignments/m24/2/Solution.java
@@ -152,8 +152,8 @@ class Bst {
         if (cmp >= m1 && cmp <= m2) {
             System.out.println((x.val).getsName());
         }
-        get(x.left, m1, m2);
         get(x.right, m1, m2);
+        get(x.left, m1, m2);
         return temp;
     }
     public String get1(final Double m1) {
@@ -166,7 +166,7 @@ class Bst {
             return null;
         }
         Double cmp = (x.val).gettMarks();
-        if (cmp < m1) {
+        if (cmp <= m1) {
             System.out.println((x.val).getsName());
         }
         get1(x.left, m1);
@@ -183,7 +183,7 @@ class Bst {
             return null;
         }
         Double cmp = (x.val).gettMarks();
-        if (cmp > m1) {
+        if (cmp >= m1) {
             System.out.println((x.val).getsName());
         }
         get2(x.left, m1);
